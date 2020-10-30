ef390dd5175a87ff7b35c7f21b1e3e36e820c3a7
David <davidaase@hotmail.com>
2020-10-31 00:43:55 +0100

    improve app-frame

--- src/main/resources/vue/components/app-frame.vue
+++ src/main/resources/vue/components/app-frame.vue
@@ -1,45 +1,50 @@
 <template id="app-frame">
     <v-app>
-        <v-app-bar app color="white" flat>
-            <v-container class="py-0 fill-height">
-                <img src="/img/logo.svg" class="navbar-logo" alt="KAT logo">
-                <v-spacer></v-spacer>
-                <v-menu v-if="$javalin.state.userInfo" bottom left offset-y>
-                    <template #activator="scope">
-                        <v-btn icon color="black" v-on="scope.on">
-                            <v-icon>mdi-account-circle</v-icon>
-                        </v-btn>
-                    </template>
-                    <v-list class="account-list">
-                        <div class="signed-in-as pa-5">
-                            Signed in as
-                            <div class="current-user">{{ $javalin.state.userInfo.id }}</div>
-                        </div>
-                        <v-list-item @click="signOut">Sign out</v-list-item>
-                    </v-list>
-                </v-menu>
-            </v-container>
-        </v-app-bar>
-        <v-main class="primary">
-            <v-container>
-                <v-row>
-                    <v-col cols="2">
-                        <v-sheet rounded="lg">
-                            <v-list color="transparent">
-                                <v-list-item link href="/" :ripple="false">Home</v-list-item>
-                                <v-list-item link href="/examples" :ripple="false">Examples</v-list-item>
-                                <v-list-item link href="/accounts" :ripple="false">Accounts</v-list-item>
-                            </v-list>
-                        </v-sheet>
-                    </v-col>
-                    <v-col>
-                        <v-sheet rounded="lg" class="pa-5">
-                            <slot></slot>
-                        </v-sheet>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-main>
+        <template v-if="$javalin.state.userInfo === null">
+            <slot></slot> <!-- don't show anything to unauthenticated users -->
+        </template>
+        <template v-if="$javalin.state.userInfo !== null">
+            <v-app-bar app color="white" flat>
+                <v-container class="py-0 fill-height">
+                    <img src="/img/logo.svg" class="navbar-logo" alt="KAT logo">
+                    <v-spacer></v-spacer>
+                    <v-menu v-if="$javalin.state.userInfo" bottom left offset-y>
+                        <template #activator="scope">
+                            <v-btn icon color="black" v-on="scope.on">
+                                <v-icon>mdi-account-circle</v-icon>
+                            </v-btn>
+                        </template>
+                        <v-list class="account-list">
+                            <div class="signed-in-as pa-5">
+                                Signed in as
+                                <div class="current-user">{{ $javalin.state.userInfo.id }}</div>
+                            </div>
+                            <v-list-item @click="signOut">Sign out</v-list-item>
+                        </v-list>
+                    </v-menu>
+                </v-container>
+            </v-app-bar>
+            <v-main class="primary">
+                <v-container>
+                    <v-row>
+                        <v-col cols="2">
+                            <v-sheet rounded="lg">
+                                <v-list color="transparent">
+                                    <v-list-item link href="/" :ripple="false">Home</v-list-item>
+                                    <v-list-item link href="/examples" :ripple="false">Examples</v-list-item>
+                                    <v-list-item link href="/accounts" :ripple="false">Accounts</v-list-item>
+                                </v-list>
+                            </v-sheet>
+                        </v-col>
+                        <v-col>
+                            <v-sheet rounded="lg" class="pa-5">
+                                <slot></slot>
+                            </v-sheet>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-main>
+        </template>
     </v-app>
 </template>
 <script>
