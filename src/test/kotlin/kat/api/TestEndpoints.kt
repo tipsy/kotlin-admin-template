4c7f708e544e03359b05671ffda4d193f92d806a
zakharovsergey1000 <zakharovsergey1000@gmail.com>
2022-10-10 13:39:18 +0300

    Added a unit test for a scenario where an HTTP client sends a non-existent client cookie.
    Updated the Config object to make it easier to set up tests.

--- src/test/kotlin/kat/api/TestEndpoints.kt
+++ src/test/kotlin/kat/api/TestEndpoints.kt
@@ -1,11 +1,13 @@
 package kat.api
 
+import kat.Config
+import kat.auth.AccountService
 import kat.auth.Role
+import kat.util.resetDatabase
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
-// You have to set environment variable USE_FAKE_LOGIN=true
-// or change Config.kt to run these tests
+// You have to set Config.useFakeLogin=true for most of the tests
 class TestEndpoints : BaseApiTest() {
 
     @Test
@@ -41,4 +43,28 @@ class TestEndpoints : BaseApiTest() {
         assertThat(response.body).contains("<sign-in-page></sign-in-page>")
     }
 
+}
+
+class TestEndpoints2 : BaseApiTest() {
+
+    // Warning: this test must not be run in parallel with other tests
+    @Test
+    fun `GET anything is redirected to login for non-existent user`() {
+        resetDatabase()
+        // we don't need fake login here
+        Config.useFakeLogin = false
+        val response = http.get("/sign-in").asString()
+        assertThat(response.status).isEqualTo(200)
+        val credentials = """{"userId":"FirstUser","password":"password"}"""
+        val response2 = http.post("/api/auth/sign-up").body(credentials).asString()
+        assertThat(response2.status).isEqualTo(201)
+        // HTTP client sends session cookie of existing user
+        val response3 = http.get("/").asString()
+        assertThat(response3.body).doesNotContain("<sign-in-page></sign-in-page>")
+        assertThat(AccountService.deleteById("FirstUser")).isTrue
+        assertThat(AccountService.findById("FirstUser")).isNull()
+        // HTTP client sends session cookie of non-existent user
+        val response4 = http.get("/").asString()
+        assertThat(response4.body).contains("<sign-in-page></sign-in-page>")
+    }
 }
